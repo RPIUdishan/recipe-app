fecfe413ce7775d93dd861fc5e9d238aaf69c01c
RPIUdishan <it18082234@my.sliit.lk>
2021-05-19 13:00:57 +0530

    add the css to header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 // import './key'
 import Axios from "axios"
+import headerIcon from './assets/images/headerIcon.png'
 
 //hi
 function App() {
+  
   const YOUR_APP_ID = "bbd18a7c";
-
   const YOUR_APP_KEY = "7aeeb949663b0c475df765e6a981bdc2";
-  var url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  
+  var url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
 
   async function getRecipes(){
     var result = await Axios.get(url);
@@ -15,8 +17,8 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1 onClick={getRecipes}>Hello Ishanka</h1>
+    <div className="app">
+      <h1 onClick={getRecipes}>Hello Ishanka <img className="headerIcon" src={headerIcon} alt="fireSpot"/></h1>
     </div>
   );
 }
