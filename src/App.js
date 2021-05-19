67d63c660d4aa1ba91a86bff51da089b7f05eff9
RPIUdishan <it18082234@my.sliit.lk>
2021-05-19 14:37:55 +0530

    update the get request url

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const YOUR_APP_ID = "bbd18a7c";
   const YOUR_APP_KEY = "7aeeb949663b0c475df765e6a981bdc2";
   
-  var url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  var url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&calories=591-722&health=alcohol-free`;
 
   async function getRecipes(){
     var result = await Axios.get(url);
